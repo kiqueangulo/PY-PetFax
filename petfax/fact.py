76b5741cc53c6157c330dbd66034f321a498fb2f
kiqueangulo <kiqueangulo@gmail.com>
2022-08-29 18:56:29 -0400

    Add POST method

--- petfax/fact.py
+++ petfax/fact.py
@@ -1,6 +1,15 @@
-from flask import (Blueprint, render_template)
+from flask import (Blueprint, render_template, request, redirect)
 
 bp = Blueprint('fact', __name__, url_prefix = '/facts')
+
+@bp.route('/', methods = ['Get', 'POST'])
+def index():
+    if request.method == 'POST':
+        print(request.form)
+
+        return redirect('/facts')
+    
+    return 'This is the facts index'
 
 @bp.route('/new')
 def new_fact():
