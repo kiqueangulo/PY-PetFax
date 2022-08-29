0febcff29695f236bad9498f9dc7be21c9fb0fa7
kiqueangulo <kiqueangulo@gmail.com>
2022-08-28 21:52:53 -0400

    Set up new fact page

--- petfax/fact.py
+++ petfax/fact.py
@@ -2,6 +2,6 @@ from flask import (Blueprint, render_template)
 
 bp = Blueprint('fact', __name__, url_prefix = '/facts')
 
-bp.route('/new')
+@bp.route('/new')
 def new_fact():
     return render_template('new_fact.html')
