380aa878eccb225a9e586a0017670b92cc75f2b3
kiqueangulo <kiqueangulo@gmail.com>
2022-08-28 20:54:42 -0400

    Establish link to show page

--- petfax/pet.py
+++ petfax/pet.py
@@ -8,3 +8,7 @@ bp = Blueprint('pet', __name__, url_prefix = '/pets')
 @bp.route('/')
 def index():
     return render_template('index.html', pets = pets)
+
+@bp.route("/<int:pet_id>")
+def show_pet(pet_id):
+    return f"This is pet {pet_id}"
